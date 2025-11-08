152e3afd2ada139f15178abde895ea28b186ad6a
JeremiahB209 <jeremiahdb209@gmail.com>
2025-11-07 22:59:17 -0800

    Create main.py

--- main.py
+++ main.py
@@ -1,5 +1,1 @@
-Make a print statement at the top of your program that displays the string "Hello World!"
 
-Ask the user for their name.
-
-Print back to the user a string with the following format: username + "is cool!"
